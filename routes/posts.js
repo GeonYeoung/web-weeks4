e9cdbe5bbe3471e341fe9a72d9bc2c5ffe231edd
GeonYeoung <kunbbang25@naver.com>
2023-11-18 18:23:37 +0900

    weeks12

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('index',{title:"계시판", pagename:"posts/list.ejs"})
+    res.render('index',{title:"게시판", pagename:"posts/list.ejs"});
 });
 
 router.get('/insert', function(req, res){
-    res.render('index',{title:"글쓰기", pagename:"posts/insert.ejs"})
+    res.render('index',{title:"글쓰기", pagename:"posts/insert.ejs"});
 });
 
+router.get('/read', function(req, res){
+    const id=req.query.id;
+    res.render('index',{title:"게시글정보", pagename:"posts/read.ejs", id});
+});
 
 module.exports = router;
